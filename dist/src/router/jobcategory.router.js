eb6a52a966083422f75cbc04d79139fc0fea8351
Rabin Shah <rabin9087@gmail.com>
2025-07-27 23:55:19 +1000

    update router

--- dist/src/router/jobcategory.router.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const jobCategory_controller_1 = require("../controller/jobCategory.controller");
-const router = (0, express_1.Router)();
-router.get("/:_id", jobCategory_controller_1.getAllJobsCategoryController);
-router.post("/", jobCategory_controller_1.createNewJobCategoryController);
-exports.default = router;
